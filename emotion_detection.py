20aa63b8962d32c01e979d780e1289f205b15d4d
Bruno <as1802130598@gmail.com>
2026-06-06 15:15:03 -0500

    emotion_detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
+def emotion_detector("I Love every desert"):
     url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
@@ -23,5 +23,4 @@ def emotion_detector(text_to_analyze):
         emotions['sadness'] = None
         emotions['dominant_emotion'] = None
     return emotions
-    if __name__ == "__main__":
 
